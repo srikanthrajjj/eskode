8f6629f6723f34dbcd9d9e2abbccf7c6f69d9609
Your Name <your-email@example.com>
2025-03-31 11:13:38 +0530

    Add deployment configuration for Railway and Netlify

--- git-commands.bat
+++ git-commands.bat
@@ -1,4 +1,4 @@
 @echo off
 "C:\Program Files\Git\bin\git.exe" add .
 "C:\Program Files\Git\bin\git.exe" commit -m "Add deployment configuration for Railway and Netlify"
-"C:\Program Files\Git\bin\git.exe" push origin main 
+"C:\Program Files\Git\bin\git.exe" push -u origin master 
